334ea51f5ad7f9ff9594caf687ab4083b34b8fb1
levimake <levimake10@gmail.com>
2020-03-24 22:32:15 +0530

    Update install_avd.sh

--- install_avd.sh
+++ install_avd.sh
@@ -18,8 +18,6 @@ export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
-echo -n "Creating repositories.cfg file"
-touch ~/.android/repositories.cfg
 
 cd tools/bin/
 ./sdkmanager --sdk_root=${ANDROID_HOME} "tools"
@@ -28,18 +26,45 @@ cd ~/Android/Sdk/tools/bin
 echo -n "Accept all the licenses"
 ./sdkmanager --licenses
 
-echo -n "Choose the platform version ( 27, 28, 29, Q, R)"
+echo
+echo
+echo
+echo -n "Creating repositories.cfg file"
+touch ~/.android/repositories.cfg
+
+echo -n "Choose the platform version ( 27, 28, 29, Q, R) : "
 read platform_ver
 
+echo
+echo
+echo
+echo
 echo -n "Geting the SDK Image"
 ./sdkmanager "system-images;android-$platform_ver;google_apis_playstore;x86_64"
 
+echo
+echo
+echo
+echo
+echo
 echo -n "Creating the emulator"
 ./avdmanager create avd -n Pixel -k "system-images;android-$platform_ver;google_apis_playstore;x86_64" --device 'pixel'
 
 echo -n "Quick Fix"
 cd ~/Android/Sdk && mkdir platforms
 
+echo
+echo
+echo
+echo
 cd ~
 echo -n "Installation Completed"
+echo
+echo
+echo
+echo
+echo
 echo -n "You may start the emulator from home directory by running the command Android/Sdk/emulator/emulator -avd Pixel"
+echo
+echo
+echo
